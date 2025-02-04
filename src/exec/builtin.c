87363efffd299cff9d18ca1192a7c2a767cbac51
lkubler <leokblr@gmail.com>
2025-02-04 13:18:44 +0100

    builtins architecture - exec>builtin.c

---
+++ src/exec/builtin.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkubler <lkubler@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 13:03:58 by lkubler           #+#    #+#             */
+/*   Updated: 2025/02/04 13:17:55 by lkubler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./include/include.h"
+
+int	is_builtin(char *cmd)
+{
+	if (ft_strcmp(cmd, "echo") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "cd") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "pwd") == 0)
+		reutrn (1);
+	if (ft_strcmp(cmd, "env") == 0)
+		return (1);
+	if (ft_strcmp(cmd, " export") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "unset") == 0)
+		return (1);
+	return (0);
+}
+
+int	dispatch_builtin(char **args, t_command cmd)
+{
+	int	result;
+
+	result = 0;
+	if (ft_strcmp(args[0], "echo") == 0)
+		result = mini_echo(args);
+	if (ft_strcmp(args[0], "cd") == 0)
+		result = mini_cd();		//woher bekomme ich env?
+	if (ft_strcmp(args[0], "pwd") == 0)
+		result = mini_pwd();
+	if (ft_strcmp(args[0], "env") == 0)
+		mini_env();
+	if (ft_strcmp(args[0], "export") == 0)
+		mini_export();
+	if (ft_strcmp(args[0], "unset") == 0)
+		mini_unset();
+}
