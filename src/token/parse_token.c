fdc6caaeb677cb9881f0e4e82983f27746a4bdd9
Lukas Seeger <lseeger@3-H-2.42heilbronn.de>
2025-02-05 15:01:55 +0100

    Refactored find space logic

--- src/token/parse_token.c
+++ src/token/parse_token.c
@@ -6,23 +6,32 @@
 /*   By: lseeger <lseeger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 15:21:57 by lseeger           #+#    #+#             */
-/*   Updated: 2025/02/05 14:19:06 by lseeger          ###   ########.fr       */
+/*   Updated: 2025/02/05 15:00:05 by lseeger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include.h"
 
-static t_token	*get_token(char **str)
+static t_token	*get_token(char **str_pos)
 {
+	char	*str;
 	char	*next;
 	t_token	*token;
 
-	next = ft_strchr(*str, ' ');
-	if (!next)
+	str = *str_pos;
+	while (*str == ' ')
+		str++;
+	if (!*str)
 		return (NULL);
+	next = str;
+	while (*next && !ft_strchr(" ", *next))
+		next++;
 	token = create_token();
-	token->str = ft_strdupn(*str, next);
-	*str = next + 1;
+	token->str = ft_strdupn(str, next);
+	if (*next)
+		*str_pos = next + 1;
+	else
+		*str_pos = next;
 	return (token);
 }
 
