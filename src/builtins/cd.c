bc794915454f7e6371b458e7c5e57bdec314705c
lkubler <leokblr@gmail.com>
2025-02-06 12:43:54 +0100

    path history für cd -

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkubler <lkubler@student.42.fr>            +#+  +:+       +#+        */
+/*   By: leokubler <leokubler@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 14:35:10 by lkubler           #+#    #+#             */
-/*   Updated: 2025/02/05 14:51:58 by lkubler          ###   ########.fr       */
+/*   Updated: 2025/02/06 12:43:28 by leokubler        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,21 @@ static int	to_path(int fl, t_command *args)
 	
 }
 
-static int	path_history(t_command *args)
+static int	path_history(char *prev_path)
 {
-	
+	static char	old_path[PATH_MAX];
+	char	current_path[PATH_MAX];
+
+	if (!prev_path)
+		return (EXIT_FAILURE);
+	if (!getcwd(current_path, sizeof(current_path)))
+		return (EXIT_FAILURE);
+	if (old_path[0] != '\0')
+		*prev_path = ft_strdup(old_path);
+	else
+		*prev_path = NULL;
+	ft_strlcpy(old_path, current_path, PATH_MAX - 1);
+	old_path[PATH_MAX - 1] = '\0';
 }
 mini_cd(char **args, t_command *env)
 {
