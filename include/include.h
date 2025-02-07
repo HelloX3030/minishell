06e0cdf63e17aa0b6e1d86ac0938dac8de893c93
lkubler <leokblr@gmail.com>
2025-02-07 13:39:40 +0100

    typo

--- include/include.h
+++ include/include.h
@@ -6,7 +6,7 @@
 /*   By: lkubler <lkubler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 13:55:06 by lseeger           #+#    #+#             */
-/*   Updated: 2025/02/07 13:36:31 by lkubler          ###   ########.fr       */
+/*   Updated: 2025/02/07 13:38:56 by lkubler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,7 +90,7 @@ typedef struct	s_env
 // builtins
 static int	to_path(int fl, t_command *args);
 static int	path_history(t_command *args);
-int			mini_cd(t_command *command);
+int			mini_cd(t_command *command, t_env *env);
 static int	count_args(char **args);
 int			mini_echo(char **args);
 void		mini_pwd(void);
