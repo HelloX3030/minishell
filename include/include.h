38dec76461797d5fddb8cf433c0db43844c37f4a
lkubler <leokblr@gmail.com>
2025-02-07 13:38:07 +0100

    header fuer env

--- include/include.h
+++ include/include.h
@@ -6,7 +6,7 @@
 /*   By: lkubler <lkubler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 13:55:06 by lseeger           #+#    #+#             */
-/*   Updated: 2025/02/07 13:35:28 by lkubler          ###   ########.fr       */
+/*   Updated: 2025/02/07 13:36:31 by lkubler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,6 +79,26 @@ typedef struct s_command
 	bool				append;
 	struct s_command	*pipe;
 }						t_command;
+
+typedef struct	s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}					t_env;
+
+// builtins
+static int	to_path(int fl, t_command *args);
+static int	path_history(t_command *args);
+int			mini_cd(t_command *command);
+static int	count_args(char **args);
+int			mini_echo(char **args);
+void		mini_pwd(void);
+
+// exec
+int		is_builtin(char *cmd);
+int		dispatch_builtin(t_command *command);
+void	execute(t_command *command);
 
 // tokens
 t_token					*create_token(t_token_type type, char *str);
