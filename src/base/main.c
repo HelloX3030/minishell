c1c68ba1b8eca4b36289a290715c6edac7866665
lkubler <leokblr@gmail.com>
2025-02-19 13:54:40 +0100

    command comment

--- src/base/main.c
+++ src/base/main.c
@@ -6,7 +6,7 @@
 /*   By: lkubler <lkubler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 15:54:04 by lseeger           #+#    #+#             */
-/*   Updated: 2025/02/19 13:53:47 by lkubler          ###   ########.fr       */
+/*   Updated: 2025/02/19 13:54:27 by lkubler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,34 +18,34 @@
 #include <string.h>
 
 
-t_command *create_test_command()
-{
-    t_command *command;
+//t_command *create_test_command()
+//{
+//    t_command *command;
 
-    command = (t_command *)malloc(sizeof(t_command));
-    if (!command)
-        return (NULL);
+//    command = (t_command *)malloc(sizeof(t_command));
+//    if (!command)
+//        return (NULL);
 
-    // Allocate memory for args array
-    command->args = (char **)malloc(sizeof(char *) * 3);  // Space for command and NULL
-    if (!command->args)
-    {
-        free(command);
-        return (NULL);
-    }
+//    // Allocate memory for args array
+//    command->args = (char **)malloc(sizeof(char *) * 3);  // Space for command and NULL
+//    if (!command->args)
+//    {
+//        free(command);
+//        return (NULL);
+//    }
 
-    command->cmd = "cd";
-    command->args[0] = "...";
-    command->args[1] = NULL;
-    command->infile = NULL;
-    command->outfile = NULL;
-    command->append = false;
-    command->pipe = false;
-    command->background = false;
-    command->next = NULL;
+//    command->cmd = "cd";
+//    command->args[0] = "...";
+//    command->args[1] = NULL;
+//    command->infile = NULL;
+//    command->outfile = NULL;
+//    command->append = false;
+//    command->pipe = false;
+//    command->background = false;
+//    command->next = NULL;
 
-    return (command);
-}
+//    return (command);
+//}
 
 int	main(int argc, char **argv, char **envp)
 {
