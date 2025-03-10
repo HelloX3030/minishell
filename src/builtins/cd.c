3e0302d59f5c361e3aeb8258c3efd6a61bb94ef4
lkubler <leokblr@gmail.com>
2025-03-10 10:22:00 +0100

    cd funciton norm

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,49 +6,17 @@
 /*   By: lkubler <lkubler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 14:35:10 by lkubler           #+#    #+#             */
-/*   Updated: 2025/02/21 13:11:07 by lkubler          ###   ########.fr       */
+/*   Updated: 2025/03/10 10:21:38 by lkubler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include.h"
 
-int to_path(int fl, t_env **env)
+static int change_directory(char *path, char *current_path, t_env **env)
 {
-	char *path;
-	char current_path[PATH_MAX];
-	static char old_path[PATH_MAX] = {0};
 	int ret;
+	char new_path[PATH_MAX];
 	
-	if (!getcwd(current_path, PATH_MAX))
-	{
-		ft_putstr_fd("minishell: cd: error retrieving current directory\n", 2);
-		return (EXIT_FAILURE);
-	}
-	if (fl == 0)
-	{
-		path = get_env_value(*env, "HOME");
-		if (!path)
-		{
-			ft_putstr_fd("minishell: cd: HOME not set\n", 2);
-			return (EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		if (!old_path[0])
-		{
-			path = get_env_value(*env, "OLDPWD");
-			if (!path)
-			{
-				ft_putstr_fd("minishell: cd: OLDPWD not set\n", 2);
-				return (EXIT_FAILURE);
-			}
-		}
-		else
-			path = old_path;
-		ft_putendl_fd(path, 1);
-	}
-	ft_strlcpy(old_path, current_path, PATH_MAX);
 	ret = chdir(path);
 	if (ret != 0)
 	{
@@ -58,13 +26,70 @@ int to_path(int fl, t_env **env)
 		perror("");
 		return (EXIT_FAILURE);
 	}
-	char new_path[PATH_MAX];
+	
 	if (getcwd(new_path, PATH_MAX))
 	{
 		set_env_val(env, "OLDPWD", current_path);
 		set_env_val(env, "PWD", new_path);
 	}
 	return (EXIT_SUCCESS);
+}
+
+static int handle_home_path(t_env **env, char *current_path)
+{
+	char *path;
+	
+	path = get_env_value(*env, "HOME");
+	if (!path)
+	{
+		ft_putstr_fd("minishell: cd: HOME not set\n", 2);
+		return (EXIT_FAILURE);
+	}
+	return (change_directory(path, current_path, env));
+}
+
+static int handle_old_path(t_env **env, char *current_path, char *old_path)
+{
+	char *path;
+	
+	if (!old_path[0])
+	{
+		path = get_env_value(*env, "OLDPWD");
+		if (!path)
+		{
+			ft_putstr_fd("minishell: cd: OLDPWD not set\n", 2);
+			return (EXIT_FAILURE);
+		}
+	}
+	else
+		path = old_path;
+	
+	ft_putendl_fd(path, 1);
+	return (change_directory(path, current_path, env));
+}
+
+int to_path(int fl, t_env **env)
+{
+	char current_path[PATH_MAX];
+	static char old_path[PATH_MAX] = {0};
+	int result;
+	
+	if (!getcwd(current_path, PATH_MAX))
+	{
+		ft_putstr_fd("minishell: cd: error retrieving current directory\n", 2);
+		return (EXIT_FAILURE);
+	}
+	char temp_path[PATH_MAX];
+	ft_strlcpy(temp_path, current_path, PATH_MAX);
+	
+	if (fl == 0)
+		result = handle_home_path(env, current_path);
+	else
+		result = handle_old_path(env, current_path, old_path);
+	if (result == EXIT_SUCCESS)
+		ft_strlcpy(old_path, temp_path, PATH_MAX);
+	
+	return (result);
 }
 
 int mini_cd(char **args, t_env **env)
