9a141c2451e79899891eb8249a9f83b49f773abb
lseeger <lseeger@student.42.fr>
2025-03-25 14:12:09 +0100

    fixed expand_env call

--- src/minishell/execute_minishell.c
+++ src/minishell/execute_minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute_minishell.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkubler <lkubler@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lseeger <lseeger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 15:31:14 by lseeger           #+#    #+#             */
-/*   Updated: 2025/03/25 13:41:37 by lkubler          ###   ########.fr       */
+/*   Updated: 2025/03/25 14:11:29 by lseeger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,16 @@ static void	execute_expression(t_minishell *ms, t_expression *expr)
 {
 	char	**args;
 
-	args = list_to_arr(ms->expr->args);
-	if (!args && ms->expr->args)
-		mini_exit(args, ms);
 	if (expand_expr_vars(expr, ms) == EXIT_FAILURE)
+		mini_exit(list_to_arr(expr->args), ms);
+	args = list_to_arr(expr->args);
+	if (!args && expr->args)
 		mini_exit(args, ms);
 	if (redirect(expr) == EXIT_FAILURE)
 		mini_exit(args, ms);
 	if (expr->type == EXPR_CMD && ft_strcmp((char *)expr->args->content,
-		"exit") == 0)
-		{
-			
-			mini_exit(args, ms);
-			// if shell_level == 0
-			//free_minishell(ms);
-			//exit(EXIT_SUCCESS);
-			//ms->stop_execution = true;
-			//return ;
-		}
+			"exit") == 0)
+		mini_exit(args, ms);
 	execute(args, ms);
 	if (reset_redirect(expr) == EXIT_FAILURE)
 		mini_exit(args, ms);
@@ -45,26 +37,3 @@ void	execute_minishell(t_minishell *ms)
 	// placeholder
 	execute_expression(ms, ms->expr);
 }
-
-//static void	execute_expression(t_minishell *ms, t_expression *expr)
-//{
-//	char	**args;
-
-//	if (expand_expr_vars(expr, ms) == EXIT_FAILURE)
-//		exit_minishell(ms, EXIT_FAILURE);
-//	args = list_to_arr(ms->expr->args);
-//	if (!args)
-//		exit_minishell(ms, EXIT_FAILURE);
-//	// error code 127 btw should maybe the normal exit code of excve be used?
-//	if (!is_cmd(*args, ms->env))
-//		exit_minishell(ms, EXIT_FAILURE);
-//	if (redirect(expr) == EXIT_FAILURE)
-//		exit_minishell(ms, EXIT_FAILURE);
-//	// obsolete
-//	if (ft_strcmp(*args, "exit") == 0)
-//		exit_minishell(ms, EXIT_SUCCESS);
-//	execute(args, ms);
-//	ft_free_strs(args);
-//	if (reset_redirect(expr) == EXIT_FAILURE)
-//		exit_minishell(ms, EXIT_FAILURE);
-//}
